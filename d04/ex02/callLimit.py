d88875d83d2e7c8d7fd53111af0c9ade7d2454e1
alex-sdn <alexsdnwcz@gmail.com>
2024-09-24 18:54:39 +0200

    add doc + flake8 norm

--- d04/ex02/callLimit.py
+++ d04/ex02/callLimit.py
@@ -1,11 +1,16 @@
 
 def callLimit(limit: int):
+    """
+    Wrapper function that takes as argument an int 'limit'
+    Functions defined with this wrapper can be called limit times max
+    """
     count = 0
-    
+
     def callLimiter(function):
         def limit_function(*args: any, **kwds: any):
+            """Executes the function if limit not reached"""
             nonlocal count
-            
+
             if count < limit:
                 count += 1
                 return function()
--- d04/ex03/new_student.py
+++ d04/ex03/new_student.py
@@ -2,11 +2,18 @@ import random
 import string
 from dataclasses import dataclass, field
 
+
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    """Generates a random 15 char long id"""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
 
 @dataclass
 class Student:
+    """
+    Dataclass with predefined variables: active, login, id
+    Takes a name and surname as argument
+    """
     name: str
     surname: str
     active: bool = field(init=False, default=True)
@@ -14,4 +21,5 @@ class Student:
     id: str = field(init=False, default=generate_id())
 
     def __post_init__(self):
+        """Sets login after __init__ was called and name + surname set"""
         self.login = self.name[0] + self.surname
