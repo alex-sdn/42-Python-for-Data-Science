98908bfc32ca5ef2d2bbcefe2962129673e89e05
alex-sdn <alexsdnwcz@gmail.com>
2024-09-24 19:00:16 +0200

    add doc + flake8 norm

--- d04/ex01/in_out.py
+++ d04/ex01/in_out.py
@@ -1,18 +1,24 @@
 
 def square(x: int | float) -> int | float:
+    """Returns the square of argument"""
     return x * x
 
 
 def pow(x: int | float) -> int | float:
+    """Returns the power of argument by itself"""
     return x ** x
 
 
 def outer(x: int | float, function) -> object:
+    """
+    Takes a number x and a function()
+    Returns an inner function that calls function(x) and updates x with the res
+    """
     count = 0
 
     def inner() -> float:
         nonlocal count, x
-        # count += 1  # useless?
+        # count += 1  # useless
 
         x = function(x)
 
