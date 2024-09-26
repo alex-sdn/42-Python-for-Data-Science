5dcff82583c8098e12bcae3528ae0d5b4f9c389f
alex-sdn <alexsdnwcz@gmail.com>
2024-09-26 20:32:39 +0200

    add copy + display img

--- d01/ex05/pimp_image.py
+++ d01/ex05/pimp_image.py
@@ -2,36 +2,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ft_invert(array):
+def ft_invert(array) -> np.ndarray:
     """Inverts the color of the image received."""
-    return 255 - array
+    inverted = 255 - array
+
+    plt.imshow(inverted)
+    plt.show()
+    return inverted
 
 
-def ft_red(array):
+def ft_red(array) -> np.ndarray:
     """Keeps only the Red channel of the image received."""
-    array[..., [1, 2]] = 0
-    return array
+    red = array.copy()
+    red[..., [1, 2]] = 0
+
+    plt.imshow(red)
+    plt.show()
+    return red
 
 
-def ft_green(array):
+def ft_green(array) -> np.ndarray:
     """Keeps only the Green channel of the image received."""
-    array[..., [0, 2]] = 0
-    return array
+    green = array.copy()
+    green[..., [0, 2]] = 0
+
+    plt.imshow(green)
+    plt.show()
+    return green
 
 
-def ft_blue(array):
+def ft_blue(array) -> np.ndarray:
     """Keeps only the Blue channel of the image received."""
-    array[..., [0, 1]] = 0
-    return array
+    blue = array.copy()
+    blue[..., [0, 1]] = 0
+
+    plt.imshow(blue)
+    plt.show()
+    return blue
 
 
-def ft_grey(array):
+def ft_grey(array) -> np.ndarray:
     """Turns the image received to greyscale."""
-    array = array.astype(float)
+    grey = array.copy().astype(float)
 
-    array[..., 0] /= 3
-    array[..., 1] /= 3
-    array[..., 2] /= 3
+    grey[..., 0] /= 3
+    grey[..., 1] /= 3
+    grey[..., 2] /= 3
 
-    grey = array.astype(np.uint8).sum(axis=-1)
-    return np.stack([grey, grey, grey], axis=2)
+    grey = grey.astype(np.uint8).sum(axis=-1)
+    grey = np.stack([grey, grey, grey], axis=2)
+
+    plt.imshow(grey)
+    plt.show()
+    return grey
