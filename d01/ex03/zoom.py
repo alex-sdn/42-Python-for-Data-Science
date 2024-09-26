deb3f2dd5be8a427775e60dee3e2bbccd98b89b8
alex-sdn <alexsdnwcz@gmail.com>
2024-09-26 15:48:20 +0200

    add main + doc

--- d01/ex03/zoom.py
+++ d01/ex03/zoom.py
@@ -1,16 +1,21 @@
 from load_image import ft_load
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
+
+def main():
+    """Loads animal.jpeg, zooms in and displays the new image"""
     pixels = ft_load("animal.jpeg")
     print(pixels)
 
     if pixels is not None:
-        # not b&w ?
-        zoomed_pixels = pixels[200:600, 400:800]
-        
-        print(f"New shape after slicing is {zoomed_pixels.shape[:3]}")
+        zoomed_pixels = pixels[150:550, 450:850]  # not b&w ?
+
+        print(f"New shape after slicing is {zoomed_pixels.shape}")
         print(zoomed_pixels)
 
         plt.imshow(zoomed_pixels)
         plt.show()
+
+
+if __name__ == '__main__':
+    main()
