c6e63b4326a0cd0a9655c6a3e411d544834673df
alex-sdn <alexsdnwcz@gmail.com>
2024-09-26 15:35:06 +0200

    add __doc__

--- d01/ex03/load_image.py
+++ d01/ex03/load_image.py
@@ -1,7 +1,12 @@
 from PIL import Image
 import numpy as np
 
-def ft_load(path: str):
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Loads the image at the given path and prints its format.
+    Returns the pixels content in RGB as a numpy.ndarray.
+    """
     try:
         img = Image.open(path).convert('RGB')
     except Exception as e:
@@ -10,6 +15,6 @@ def ft_load(path: str):
 
     pixels = np.array(img)
 
-    print(f"The shape of the image is {pixels.shape[:3]}")
+    print(f"The shape of the image is {pixels.shape}")
 
     return pixels
