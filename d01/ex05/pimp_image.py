f343d3090c674e85936dee6488777ec4a3541ad6
alex-sdn <alexsdnwcz@gmail.com>
2024-09-26 20:22:16 +0200

    add doc

--- d01/ex05/pimp_image.py
+++ d01/ex05/pimp_image.py
@@ -1,24 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def ft_invert(array):
-    return 255 - array 
+    """Inverts the color of the image received."""
+    return 255 - array
+
 
 def ft_red(array):
+    """Keeps only the Red channel of the image received."""
     array[..., [1, 2]] = 0
     return array
 
+
 def ft_green(array):
+    """Keeps only the Green channel of the image received."""
     array[..., [0, 2]] = 0
     return array
 
+
 def ft_blue(array):
+    """Keeps only the Blue channel of the image received."""
     array[..., [0, 1]] = 0
     return array
 
+
 def ft_grey(array):
+    """Turns the image received to greyscale."""
     array = array.astype(float)
-    
+
     array[..., 0] /= 3
     array[..., 1] /= 3
     array[..., 2] /= 3
