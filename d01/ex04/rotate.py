986e5c14b99089fcca482ed35f601aa126165e5b
alex-sdn <alexsdnwcz@gmail.com>
2024-09-26 20:14:06 +0200

    add rotate() func + doc

--- d01/ex04/rotate.py
+++ d01/ex04/rotate.py
@@ -2,16 +2,34 @@ from load_image import ft_load
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
+
+def rotate(pixels):
+    """Rotates a numpy array by 90 degree"""
+    rot_pixels = pixels.copy()
+    size = len(pixels)
+
+    for i in range(size):
+        for j in range(size):
+            rot_pixels[size - j - 1][i] = pixels[i][j]
+
+    return rot_pixels
+
+
+def main():
+    """Loads the img, resizes and rotates it before displaying"""
     pixels = ft_load("animal.jpeg")
     print(pixels)
 
     if pixels is not None:
         zoomed_pixels = pixels[200:600, 400:800]
-        rotated_pixels = np.rot90(zoomed_pixels)
+        rotated_pixels = rotate(zoomed_pixels)
 
-        print(f"New shape after transpose is {rotated_pixels.shape[:3]}")
+        print(f"New shape after transpose is {rotated_pixels.shape}")
         print(rotated_pixels)
 
         plt.imshow(rotated_pixels)
         plt.show()
+
+
+if __name__ == '__main__':
+    main()
