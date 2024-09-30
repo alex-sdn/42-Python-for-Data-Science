35e83752fc2754c849bcc187b43bac2b15576358
alex-sdn <alexsdnwcz@gmail.com>
2024-09-30 15:42:01 +0200

    add doc + error mgmt

--- d01/ex00/give_bmi.py
+++ d01/ex00/give_bmi.py
@@ -1,16 +1,26 @@
-import numpy # ???
 
+def give_bmi(height: list[int | float], weight: list[int | float]):
+    """
+    Takes 2 lists of ints or floats of the same size
+    and returns a list of BMI values.
+    """
+    try:
+        if len(height) != len(weight):
+            raise AssertionError('Lists need to be of same size')
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    if len(height) != len(weight):
-        print("Lists need to be of same size")  # AssertError ?
+        return [w / (h * h) for h, w in zip(height, weight)]
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
         return None
-    # check if both int / float ?
-
-    return [w / (h * h) for h, w in zip(height, weight)]
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    # error check ?
-
-    return [item > limit for item in bmi]
+    """
+    Takes a list of BMI values and a limit, returns a list of booleans
+    with True if the value is above the limit.
+    """
+    try:
+        return [item > limit for item in bmi]
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+        return None
