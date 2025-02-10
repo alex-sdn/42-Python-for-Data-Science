662928337360bb629a55689c7bf4d3534c8e0cd3
Alexandre S <alexsdnwcz@gmail.com>
2025-02-10 13:33:57 +0100

    make grayscale

--- d01/ex03/zoom.py
+++ d01/ex03/zoom.py
@@ -1,10 +1,12 @@
 from load_image import ft_load
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def main():
     """Loads animal.jpeg, zooms in and displays the new image"""
     pixels = ft_load("animal.jpeg")
+    pixels = pixels[:, :, 0:1]
     print(pixels)
 
     if pixels is not None:
@@ -13,7 +15,7 @@ def main():
         print(f"New shape after slicing is {zoomed_pixels.shape}")
         print(zoomed_pixels)
 
-        plt.imshow(zoomed_pixels)
+        plt.imshow(zoomed_pixels, cmap='gray')
         plt.show()
 
 
